9391037b5e8fd8c096b558a165a3b2b15383bedd
unknown <sktang@697-334-Erebor.sf.k12.internal>
2015-09-01 11:29:28 -0700

    Made one lightning bolt

--- Lightning.pde
+++ Lightning.pde
@@ -1,30 +1,39 @@
 int startX=0;
-int startY=150;
+int startY=200;
 int endX=0;
-int endY=150;
+int endY=200;
 int r;
 int g;
 int b;
 void setup()
 {
   size(300,300);
-  strokeWeight(20);
-  background(r,g,b);
+  strokeWeight(2);
+  background(234,43,143);
   size(400,400);
 }
 void draw()
 {
 	while(endX<400) {
-	int r = (int)(Math.random()*256);
-	int b = (int)(Math.random()*256);
-	int g = (int)(Math.random()*256);
-	int light=(int)(Math.random()*256);
-	stroke(light);
-}
-
+		int r = (int)(Math.random()*256);
+		int b = (int)(Math.random()*256);
+		int g = (int)(Math.random()*256);
+		int rX = (int)(Math.random()*9);
+		int rY = (int)(Math.random()*18)-9;
+		stroke(r,g,b);
+		endX=startX+rX;
+		endY=startY+rY;
+		line(startX,startY,endX,endY);
+		startX=endX;
+		startY=endY;
+	}
+	mousePressed();
 }
 void mousePressed()
 {
-
+	int startX=0;
+	int startY=200;
+	int endX=0;
+	int endY=200;
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -15,27 +15,43 @@ import java.io.IOException;
 public class Lightning extends PApplet {
 
 int startX=0;
-int startY=150;
+int startY=200;
 int endX=0;
-int endY=150;
+int endY=200;
 int r;
 int g;
 int b;
 public void setup()
 {
   size(300,300);
-  strokeWeight();
-  background(r,g,b);
+  strokeWeight(2);
+  background(234,43,143);
+  size(400,400);
 }
 public void draw()
 {
-	r = (int)(Math.random()*256);
-	b = (int)(Math.random()*256);
-	g = (int)(Math.random()*256);
+	while(endX<400) {
+		int r = (int)(Math.random()*256);
+		int b = (int)(Math.random()*256);
+		int g = (int)(Math.random()*256);
+		int rX = (int)(Math.random()*9);
+		int rY = (int)(Math.random()*18)-9;
+		stroke(r,g,b);
+		endX=startX+rX;
+		endY=startY+rY;
+		line(startX,startY,endX,endY);
+		startX=endX;
+		startY=endY;
+	}
+	mousePressed();
 }
 public void mousePressed()
 {
-
+	int startX=0;
+	int startY=200;
+	int endX=0;
+	int endY=200;
+	loop();
 }
 
   static public void main(String[] passedArgs) {
